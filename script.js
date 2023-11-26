9fa7ac99ff184e2e1427ce8620d8d939ac4aee3e
sneha kumari <snehakumari64445@gmail.com>
2023-11-26 20:44:26 +0530

    yo

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+mycanvas.width=400;
+mycanvas.height=300;
+const margin=30;
 const n = 20;
 const array = [];
 for (let i = 0; i < n; i++) {
     array[i] = Math.random();
 }
 const cols = [];
-const spacing = mycanvas.width / n;
+const spacing = (mycanvas.width-margin*2) / n;
 const ctx = mycanvas.getContext("2d");
 for (let i = 0; i < array.length; i++) {
-    const x = i * spacing + spacing / 2;
-    const y = mycanvas.height;
-    const width = spacing;
-    const height = mycanvas.height * array[i];
+    const x = i * spacing + spacing / 2+margin;
+    const y = mycanvas.height-margin;
+    const width = spacing-4;
+    const height = (mycanvas.height-margin *2)* array[i];
     cols[i] = new Column(x, y, width, height);
     cols[i].draw(ctx);
 }
